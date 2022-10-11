edb7fedc37adb5e4b53661a8105dc65c2c26485f
lutfi nuril anwar <lutfi.nurilanwar@gmail.com>
2022-10-11 14:42:58 +0700

    feature: catch action - catch page add catch action, now can select moves to interact with pokemon, catch them all

--- pages/catch/[name].js
+++ pages/catch/[name].js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const CatchName = () => {
@@ -7,10 +7,63 @@ const CatchName = () => {
   const router = useRouter();
   const { name } = router.query;
   const [level, setLevel] = useState(0);
+  const [hitpoint, setHitpoint] = useState(0);
+  const [maxhitpoint, setmaxHitpoint] = useState(0);
+  const [catchRate, setcatchRate] = useState(0);
+  const [weak, setWeak] = useState(false);
+  const [evasive, setEvasive] = useState(60);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleThrowPokeball = () => {
+    let chance = Math.floor(Math.random() * (0 + 100) + 10);
+    if (chance < catchRate) {
+      alert(`${data.species} caught!`);
+      Router.replace("/");
+    } else {
+      alert(`${data.species} broke the pokeball!`);
+    }
+  };
+
+  const handleThrowStone = () => {
+    let chance = Math.floor(Math.random() * (0 + 100));
+    if (chance < evasive) {
+      let damage = Math.floor(Math.random() * (15 - 8) + 10);
+      if (hitpoint - damage > 0) {
+        setHitpoint(hitpoint - damage);
+        setcatchRate(parseFloat((catchRate = catchRate + 40 / level)));
+        document.getElementById("sprite").className = "damaged";
+        setTimeout(() => {
+          document.getElementById("sprite").className = "image";
+        }, 500);
+        if ((hitpoint / maxhitpoint) * 100 < 55 && weak == false) {
+          setcatchRate(parseFloat((catchRate = catchRate + 15)));
+          setWeak(true);
+        }
+        if ((hitpoint / maxhitpoint) * 100 < 20 && weak == true) {
+          setcatchRate(parseFloat((catchRate = catchRate + 25)));
+        }
+      } else if (hitpoint - damage <= 0) {
+        setHitpoint(0);
+        alert(`${name} fainted`);
+        Router.replace("/");
+      }
+    } else {
+      alert(`throw missed!`);
+    }
+  };
+
+  const handleThrowBerry = () => {
+    alert(`${name} eat the berry!`);
+    setEvasive(evasive + 20);
+  };
+
+  const handleRun = () => {
+    alert(`run away safely`);
+    Router.replace("/");
+  };
 
   async function fetchData() {
     setLoading(true);
@@ -49,21 +102,37 @@ const CatchName = () => {
     return <div className="loading">no data</div>;
   } else {
     return (
-      <div className="catchpokemon">
+      <div
+        className="catchpokemon"
+        onLoad={() => {
+          setHitpoint(data.baseStats.hp + level * 2),
+            setmaxHitpoint(data.baseStats.hp + level * 2),
+            setcatchRate(
+              data.catchRate.percentageWithOrdinaryPokeballAtFullHealth
+            );
+        }}
+      >
         <div className="catchpokemon_info">
           <div className="catchpokemon_desc">
             <p className="catchpokemon_name">{data.species}</p>
             <p className="level">Lv{level}</p>
+            {/* <p className="level">{catchRate}</p> */}
           </div>
           <div className="catchpokemon_hp">
-            <div className="status" style={{ ["width"]: `100%` }}>
+            <div
+              className="status"
+              style={{
+                ["width"]: `${(hitpoint / maxhitpoint) * 100}%`,
+              }}
+            >
               <p>HP</p>
-              <p>{data.baseStats.hp + level * 2}</p>
+              <p>{hitpoint}</p>
             </div>
           </div>
         </div>
         <div className="catchpokemon_sprite">
           <img
+            id="sprite"
             className="image"
             alt=""
             src={
@@ -74,10 +143,22 @@ const CatchName = () => {
           />
         </div>
         <div className="catchpokemon_action">
-          <p>Throw Pokeball</p>
-          <p>Throw Berry</p>
-          <p>Throw Stone</p>
-          <p>Run</p>
+          <div>
+            <a onClick={() => handleThrowPokeball()} className="mx-2">
+              <p>Throw Pokeball</p>
+            </a>
+            <a onClick={() => handleThrowBerry()}>
+              <p>Throw Berry</p>
+            </a>
+          </div>
+          <div>
+            <a onClick={() => handleThrowStone()}>
+              <p>Throw Stone</p>
+            </a>
+            <a onClick={() => handleRun()}>
+              <p>Run</p>
+            </a>
+          </div>
         </div>
       </div>
     );
